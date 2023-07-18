d6cec3e56240e42a39ffb29afafddf6fe5cedbc1
yujinjjang2 <https://github.com/yujinjjang2>
2023-07-18 16:46:42 +0900

    academy update

--- src/edu/kh/control/loop/ForExample.java
+++ src/edu/kh/control/loop/ForExample.java
@@ -63,11 +63,208 @@ public class ForExample {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("수 입력 : ");
+		System.out.print("입력 번호 : ");
 		int input = sc.nextInt();
 		
 		for(int i = 1; i <= input; i++) {
 			System.out.println(i);
+		}
+	}
+	
+	public void ex5() {
+		// 1부터 입력받은 수까지 2씩 증가하며 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력 번호 : ");
+		int input = sc.nextInt();
+		
+		for(int i = 1; i <= input; i += 2) {
+			System.out.println(i);
+		}
+	}
+	
+	public void ex6() {
+		// 1.0부터 입력 받은 실수 까지 0.5씩 증가하며 출력
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력 번호 : ");
+		double input = sc.nextDouble();
+		
+		for(double i = 1.0; i <= input; i += 0.5) {
+			System.out.println(i);
+		}
+	}
+	
+	public void ex7() {
+		// 영어 알파벳 A 부터 Z 까지 한줄로 출력
+		// ABCDEF...Z
+		
+		// * char 자료형은 문자형이지만 실제로 정수(문자표 번호)를 저장한다
+		
+		for(int i = 'A'; i <= 'Z'; i++) {
+			System.out.print( (char)i ); // 강제형변환, ABCDEFGHIJKLMNOPQRSTUVWXYZ
+		}
+		
+		System.out.println("\n-------------------------------------");
+		
+		for(char i = 'A'; i <= 'Z'; i++) {
+			System.out.print(i); // ABCDEFGHIJKLMNOPQRSTUVWXYZ
+		}
+	}
+	
+	public void ex8() {
+		// 10에서 1까지 1씩 감소하며 출력
+		
+		for(int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+	}
+	
+	public void ex9() {
+		
+		// for문 응용 1 : 반복문을 이용한 값의 누적
+		
+		// 1부터 10까지 모든 정수의 합 구하기
+		
+		int sum = 0; // 반복되어 증가되는 i값의 합계를 저장할 변수 
+					    // 0으로 시작하는 이유 : 아무것도 더하지 않음으로
+							                // 정확히 결과를 도출할 수 있기 때문
+		
+		for(int i = 1; i <= 10; i++) {
+			
+			// sum = sum + i;
+			sum += i;
+			// 0 = 0 + 1
+			// 1 = 1 + 1...
+		}
+		
+		System.out.println("합계 : " + sum); // 합계 : 55
+		
+	}
+	
+	public void ex10() {
+		// for문 응용 2 : 정수 5개를 입력 받아서 합계 구하기
+		
+		// ex)
+		// 입력 1 : 10
+		// 입력 2 : 20
+		// 입력 3 : 30
+		// 입력 4 : 40
+		// 입력 5 : 50
+		// 합계 : 150
+		
+		/*Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력 1 : ");
+		int input1 = sc.nextInt();
+		
+		System.out.print("입력 2 : ");
+		int input2 = sc.nextInt();
+		
+		System.out.print("입력 3 : ");
+		int input3 = sc.nextInt();
+		
+		System.out.print("입력 4 : ");
+		int input4 = sc.nextInt();
+		
+		System.out.print("입력 5 : ");
+		int input5 = sc.nextInt();
+		
+		int result = input1 + input2 + input3 + input4 + input5;
+		
+		System.out.printf("합계 : %d\n", result);*/
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= 5; i++) {
+			System.out.print("입력" + i + " : ");
+			int input = sc.nextInt();
+			
+			sum+= input; // sum에 입력받은 input값 누적
+		}
+		
+		System.out.println("합계 : " + sum);
+		
+	}
+	
+	public void ex11() {
+		// 정수를 몇 번 입력 받을지 먼저 입력 받고
+		// 입력된 정수의 합계를 출력
+		
+		// ex)
+		// 입력 받을 정수의 개수 : 3
+		// 입력 1 : 10
+		// 입력 2 : 20
+		// 입력 3 : 30
+		// 합계 : 60
+		
+		// ex)
+		// 입력 받을 정수의 개수 : 2
+		// 입력 1 : 10
+		// 입력 2 : 20
+		// 합계 : 30
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력 받을 정수의 개수 : ");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= num; i++) {
+			System.out.print("입력" + i + " : ");
+			int input = sc.nextInt();
+			
+			sum += input;
+		}
+		
+		System.out.println("합계 : " + sum);
+		
+		
+	}
+	
+	public void ex12() {
+		// 1부터 20까지 1씩 증가하면서 출력
+		// 단, 5의 배수 () 를 붙여서 출력
+		// ex) 1 2 3 4 (5) 6 7 8 9 (10) 11 ... (20)
+		
+		for(int i = 1; i <= 20; i++) {
+			
+			if (i % 5 == 0) {
+				System.out.print("(" + i + ") ");
+			} else {
+				System.out.print(i + " "); // 1 2 3 4 (5) 6 7 8 9 (10) 11 12 13 14 (15) 16 17 18 19 (20) 
+			}
+		}
+	}
+	
+	public void ex13() {
+		// 1부터 20까지 1씩 증가하면서 출력
+		// 단, 입력 받은 수의 배수는 () 표시
+		
+		// ex)
+		// 괄호를 표시할 배수 : 3
+		// 1 2 (3) 4 5 (6) ...
+		
+		// 괄호를 표시할 배수 : 4
+		// 1 2 3 (4) 5 6 7 (8) 9 ...
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("괄호를 표시할 배수 : ");
+		int num = sc.nextInt();
+		
+		for(int i = 1; i <= 20; i++) {
+			
+			if (i % num == 0) {
+				System.out.print("(" + i + ") ");
+			} else {
+				System.out.print(i + " ");
+			}
 		}
 	}
 
--- src/edu/kh/control/loop/LoopRun.java
+++ src/edu/kh/control/loop/LoopRun.java
@@ -9,7 +9,16 @@ public class LoopRun {
 		//forEx.ex1();
 		//forEx.ex2();
 		//forEx.ex3();
-		forEx.ex4();
+		//forEx.ex4();
+		//forEx.ex5();
+		//forEx.ex6();
+		//forEx.ex7();
+		//forEx.ex8();
+		//forEx.ex9();
+		//forEx.ex10();
+		//forEx.ex11();
+		//forEx.ex12();
+		forEx.ex13();
 	}
 
 }
